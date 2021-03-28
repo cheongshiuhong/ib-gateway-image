6960d8ce7a4465362ead09d7b4f914d72e9a39dc
cheongshiuhong <cheongshiuhong.git@gmail.com>
2021-03-28 23:51:54 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ def home():
     print('DB USER:', os.environ.get('DB_USER'))
     print('DB PASS:', os.environ.get('DB_PASS'))
     print('DB NAME:', os.environ.get('DB_NAME'))
-    print('DB SOCKET DIR:'. os.environ.get('DB_SOCKET_DIR', '/cloudsql'))
-    print('CLOUD SQL CONNECTION NAME:'. os.environ.get('CLOUD_SQL_CONNECTION_NAME'))
+    print('DB SOCKET DIR:', os.environ.get('DB_SOCKET_DIR', '/cloudsql'))
+    print('CLOUD SQL CONNECTION NAME:', os.environ.get('CLOUD_SQL_CONNECTION_NAME'))
     return {'Test': 'It works!'}
 
 @app.route('/run')
